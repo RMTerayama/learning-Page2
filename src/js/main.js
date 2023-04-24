23411782d59181328422d22cea29e10d88ef5617
Richard Masayuki Terayama <85762067+RMTerayama@users.noreply.github.com>
2023-04-24 19:38:48 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,26 @@ var cont =1;
 VanillaTilt.init(document.querySelectorAll(".vantagens"));
 VanillaTilt.init(document.querySelectorAll(".linguagem"));
 
+
+new TypeIt("#animated", {
+    speed: 150,
+    loop: true,
+
+  })  
+  .type("HTML", { delay: 800 })
+  .delete(4)
+  .type("CSS",{ delay: 800 })
+  .delete(3)
+  .type("User Interface (UI)", { delay: 800 })
+  .delete(19)
+  .type("Responsive Design",{ delay: 800 })
+  .delete(17)
+  .type("Front-End",{ delay: 800 })
+  .pause(1500)
+  .delete(9)
+  .go();
+
+
 setInterval(()=>{
     proximoComent();
 },3500);
